f9cfb632d8594dc1f945c7f03ff3724a9906b75e
jj196 <jjlima.jjl@gmail.com>
2021-06-07 11:13:33 -0600

    Update 01_reflex_agent.js

--- 01_reflex_agent.js
+++ 01_reflex_agent.js
@@ -1,38 +1,37 @@
 // MIT License
 // Copyright (c) 2020 Luis Espino
 
-function reflex_agent(location, state) {
-    if (state == "DIRTY") return "CLEAN";
-    else if (location == "A") return "RIGHT";
-    else if (location == "B") return "LEFT";
+function reflex_agent(location, state){
+    if (state=="DIRTY") return "CLEAN";
+    else if (location=="A") return "RIGHT";
+    else if (location=="B") return "LEFT";
 }
 
-function test(states) {
-    for (i in states) {
-        var state = states[i] 
-        var location = state[0];
-        var state = state[0] == "A" ? state[1] : state[2];
-        var action_result = reflex_agent(location, state);
-        document.getElementById("log").innerHTML += "<br>Location: ".concat(location).concat(" | Action: ").concat(action_result);
-        if (action_result == "CLEAN") {
-            if (location == "A") state[1] = "CLEAN";
-            else if (location == "B") state[2] = "CLEAN";
-        }
-        else if (action_result == "RIGHT") state[0] = "B";
-        else if (action_result == "LEFT") state[0] = "A";
-    }
-
-    setTimeout(function () { test(states); }, 2000);
+function test(states){
+       var location = states[0];		
+       var state = states[0] == "A" ? states[1] : states[2];
+       var action_result = reflex_agent(location, state);
+       document.getElementById("log").innerHTML+="<br>Location: ".concat(location).concat(" | Action: ").concat(action_result);
+       if (action_result == "CLEAN"){
+         if (location == "A") states[1] = "CLEAN";
+          else if (location == "B") states[2] = "CLEAN";
+       }
+       else if (action_result == "RIGHT") states[0] = "B";
+       else if (action_result == "LEFT") states[0] = "A";		
+ setTimeout(function(){ test(states); }, 2000);
 }
 
 var states = [
-    ["A", "DIRTY", "DIRTY"],
-    ["B", "DIRTY", "DIRTY"],
-    ["A", "DIRTY", "CLEAN"],
-    ["B", "CLEAN", "DIRTY"],
-    ["A", "CLEAN", "DIRTY"],
-    ["B", "DIRTY", "CLEAN"],
-    ["A", "CLEAN", "CLEAN"],
-    ["B", "CLEAN", "CLEAN"]
+    ["A","DIRTY","DIRTY"],
+    ["B","DIRTY","DIRTY"],
+    ["A","DIRTY","CLEAN"],
+    ["B","CLEAN","DIRTY"],
+    ["A","CLEAN","DIRTY"],
+    ["B","DIRTY","CLEAN"],
+    ["A","CLEAN","CLEAN"],
+    ["B","CLEAN","CLEAN"]
 ];
-test(states);
+for(var i in states){
+    test(states[i]);
+}
+
